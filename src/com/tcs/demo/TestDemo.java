b3574fa5137ad7087ed02aaf2373e594bcff9ece
Sana <sanabasheer83@gmail.com>
2022-03-21 22:30:12 -0700

    initial commit for java project

---
+++ src/com/tcs/demo/TestDemo.java
@@ -0,0 +1,10 @@
+package com.tcs.demo;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("demo inside test ");
+	}
+
+}
